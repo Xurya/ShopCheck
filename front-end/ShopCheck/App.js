0ddc0988aa0e814aab79fcb3f941fb76fd71a026
xurya <ryanxu@buffalo.edu>
2020-07-03 16:38:36 -0400

    PreExpo dependency fix commit

--- front-end/ShopCheck/App.js
+++ front-end/ShopCheck/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Platform, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform, StatusBar, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; //This is to support react-navigation wrapper
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator(); 
 
 export default function App() {
 
   return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "ShopCheck" }}/>
+          <Stack.Screen name="Registration" component={RegistrationScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+function HomeScreen({navigation}){
+  return(
     <SafeAreaView style={styles.container}>
-        <Image style={styles.img} source={require('./assets/logo.png')}/>
+      <Text>ShopCheck</Text>
+      <Image style={styles.img} source={require('./assets/logo.png')}/>
+
+      <Button title="Registration" onPress={()=>navigation.push("Registration")} />
+    </SafeAreaView> 
+  );
+}
+
+function RegistrationScreen({navigation}){
+  return (
+    <SafeAreaView>
+      <Text>Registration Screen</Text>
     </SafeAreaView>
   );
 }
