073e175c57e2cdecc3b943daf058dbac078a44a1
Conan <conanwon@buffalo.edu>
2020-07-10 21:03:14 -0400

    Added in insertUser and readUser to create/access user info in the db

--- back-end/mongo/server_connect.js
+++ back-end/mongo/server_connect.js
@@ -2,21 +2,66 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
-
+// const url = 'mongodb://localhost:27017';
 // Database Name
-const dbName = 'myproject';
-
+// const dbName = 'myproject';
 // Create a new MongoClient
-const client = new MongoClient(url);
+// const client = new MongoClient(url);
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+const uri = "mongodb+srv://user_01:Anh3K6yBolbqyKmo@shopcheck.3ltnq.mongodb.net/ShopCheck?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  const db = client.db(dbName);
+// Use connect method to test connection to database.
+function ConnectionTest(){
+  client.connect(function(err,client) {
+    if (err) console.error(err);
+    console.log("Connected successfully to server");
+    client.close();
+  });
+}
 
-  client.close();
-});
+// To Do: Move to separate user class for full Users CRUD
+// Find user in database
+function readUser(username,callback){
+  client.connect(function(err,cli){
+    const db = cli.db('ShopCheck');
+    const collection = db.collection('Users');
 
+    collection.find({username}).limit(1).each((err,entry)=>{
+      assert.equal(null,err);
+      client.close();
+
+
+      if(entry) {
+        callback(entry);
+        return false;
+      }
+      callback(null);
+    })
+  })
+}
+
+// Insert new user to database
+function insertUser(user){
+  client.connect(function(err,cli){
+    const db = cli.db('ShopCheck');
+    const collection = db.collection('Users');
+
+    collection.insertOne({'username':user['username'],'password':user['password'],'email':user['email']}, function(err,r){
+      assert.equal(null,err);
+      assert.equal(1,r.insertedCount);
+
+      client.close();
+    })
+  })
+}
+
+//insertUser({'username':'hellggo','password':'abcdefg','email':'yuhh@gmail.com'});
+//readUser('hello',entry=>{console.log(entry)});
+//readUser('hellggo',entry=>{console.log(entry)});
+
+module.exports={
+  ConnectionTest:ConnectionTest,
+  insertUser:insertUser,
+  readUser:readUser
+}
