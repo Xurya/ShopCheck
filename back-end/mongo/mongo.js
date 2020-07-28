8561beb005bc5c3ce2930b47f22d74d83dac0cfd
xurya <ryanxu@buffalo.edu>
2020-07-27 23:00:40 -0400

    Orders CRUD

--- back-end/mongo/mongo.js
+++ back-end/mongo/mongo.js
@@ -2,6 +2,7 @@ const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://user_02:qmZKKxnWhDTFt8rY@shopcheck.3ltnq.mongodb.net/ShopCheck?retryWrites=true&w=majority";
 const Users = require ('./users');
 const Shops = require('./shop');
+const Order = require('./order')
 const Configs = require ('./config.js');
 
 class MongoU {
@@ -15,6 +16,18 @@ class MongoU {
         this.Users = new Users(this.db);
         this.Configs = new Configs(this.db);
         this.Shops = new Shops(this.db);
+        this.Order = new Order(this.db);
+        
+        //Order Testing
+        // let _id = await this.Order.addOrder({'shopID': 'testShop','username': 'testotest','items': ["ice-cream", "snaccs", "pizza"]});
+        // var ObjectId = require('mongodb').ObjectID;
+        // console.log (new ObjectId(_id));
+        // let orders = await this.Order.getOrders({_id:new ObjectId(_id)});
+        // console.log(orders);
+        // orders = await this.Order.getAllOrders("testotest");
+        // console.log(orders);
+        // await this.Order.updateOrder(_id, {field:"items",  value:["ice-cream", "sandwich", "snaccs", "doggo"]})
+
         await this.Configs.init();
     }
 }
---
+++ back-end/mongo/order.js
@@ -0,0 +1,33 @@
+var ObjectId = require('mongodb').ObjectID;
+
+class Order {
+    constructor(db) {
+        this.collection = db.collection('Order');
+    }
+    async addOrder(order){
+        let val = await this.collection.insertOne(
+            {
+                'shopID:':order['shopID'],
+                'username':order['username'],
+                'status':false,
+                'items': order["items"]
+            });
+        return val.insertedId; //This should return the _id / order#
+    }
+    async getOrders(query){
+        let orders = await this.collection.find(query).toArray();
+        return orders;
+    }
+    async getAllOrders(username){
+        let orders = await this.collection.find({username: username}).toArray();
+        return orders;
+    }
+    async updateOrder(order_id, changeObj){
+        let changes = {};
+        changes[changeObj['field']] = changeObj['value'];
+        let val = await this.collection.updateOne({'_id':new ObjectId(order_id)},{$set:changes},{upsert:false});
+        return val;
+    }
+}
+
+module.exports = Order
