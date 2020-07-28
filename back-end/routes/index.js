a04341eaec68513879ab51dec8e847787e265ed9
xurya <ryanxu@buffalo.edu>
2020-07-28 16:12:01 -0400

    index.js fix

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -4,6 +4,6 @@ const orders = require('./order');
 
 module.exports = app => {
     app.use('/account', users.router);
-    app.use('/shops', shops.router
-    app.use("/order", orders)
+    app.use('/shops', shops.router);
+    app.use("/order", orders);
 }
